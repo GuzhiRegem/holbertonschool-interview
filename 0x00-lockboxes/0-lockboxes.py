be54bb6cee62e216509db9dfff5058a7e164276f
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-04-18 21:01:56 -0300

    commit

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    
+    module
+    return: nothing
+"""
+
+
+def canUnlockAll(boxes):
+    visited = [0]
+    stack = [0]
+    while(stack != []):
+        idx = stack[-1]
+        found = -1
+        for key in boxes[idx]:
+            if key in visited:
+                continue
+            found = key
+            break
+        if found == -1:
+            stack.pop()
+        else:
+            stack.append(found)
+            visited.append(found)
+        return len(visited) == len(boxes)
