7a035efc79597f31f006fb1842414498b3846306
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-08-11 12:45:10 -0300

    commit

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_node - func
+ * @head: list
+ * @number: new_node
+ * Return: list
+ **/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *pr, *nx = NULL, *new;
+	int n = number;
+
+	pr = *head;
+	if (pr)
+	{
+		if (pr->n > n)
+		{
+			nx = pr;
+			pr = NULL;
+		}
+		else
+		{
+			for (nx = pr->next; nx && nx->n <= n; pr = nx, nx = pr->next)
+				;
+		}
+	}
+	new = malloc(sizeof(listint_t *));
+	if (!new)
+		return (NULL);
+	new->n = number;
+	new->next = nx;
+	if (pr)
+		pr->next = new;
+	else
+		*head = new;
+	return (new);
+}
