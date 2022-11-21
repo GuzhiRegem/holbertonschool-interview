4681f84ac7dd32f468c54927f120be957e227386
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-11-21 14:33:21 -0300

    commit

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const request = require('request');
+const movieID = process.argv[2];
+
+function doRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error && res.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+const main = async () => {
+  const body = await doRequest('https://swapi-api.hbtn.io/api/films/' + movieID);
+  const obj = JSON.parse(body);
+  for (let i = 0; obj.characters[i]; i++) {
+    const character = JSON.parse(await doRequest(obj.characters[i]));
+    console.log(character.name);
+  }
+};
+if (movieID !== undefined) {
+  try {
+    main();
+  } catch (e) {
+    return;
+  }
+}
